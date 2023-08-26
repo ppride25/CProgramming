6fe61e8551324e401bb8dae42b0ba3636a6b61b9
ppride25 <pingkoyletics@gmail.com>
2023-08-26 02:13:05 +0200

    if

--- Week1/hello.c
+++ Week1/hello.c
@@ -2,5 +2,22 @@
 
 int main(void)
 {
-    printf("Hello World \n");
+    int x = 2;
+    int y = 1;
+
+    if (x>y)
+    {
+        printf("x is greater than y \n");
+    }
+    else if (x<y)
+    {
+        printf("x isn't greater than y \n");
+    }
+    else if (x==y)
+    {
+        printf ("x is equal to y \n");
+    }
+    
+    
+
 }
